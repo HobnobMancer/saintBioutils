75e56452e61f0c18f73c396c0199b752918144b6
HobnobMancer <hobnobmancer@Paddington.localdomain>
2022-01-30 23:00:45 +0000

    update version

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with Path("README.md").open("r") as long_description_handle:
 
 setuptools.setup(
     name="saintBioutils",
-    version="0.0.16",
+    version="0.0.17",
     # Metadata
     author="Emma E. M. Hobbs",
     author_email="eemh1@st-andrews.ac.uk",
